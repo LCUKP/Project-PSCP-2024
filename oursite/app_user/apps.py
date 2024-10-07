425c5b1e4495f1e3656ba21bb8cc620055d9ffe4
LCUKP <puwanatjaijitjaam@gmail.com>
2024-10-08 02:29:50 +0700

    restructure

--- oursite/app_user/apps.py
+++ oursite/app_user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BookingConfig(AppConfig):
+class AppUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'booking'
+    name = 'app_user'
--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^+ed=d3rcay+wnfe@d@=@ytb69^uv9_62l5#hq(pu$i^+d&ku'
+SECRET_KEY = 'django-insecure-_f0#@3nox+b-pqgzrk#9k#kai)ks+4^g0d+j7bo3mku_n(-&a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_user'
 ]
 
 MIDDLEWARE = [
