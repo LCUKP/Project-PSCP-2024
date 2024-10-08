1d8b31b74dfcb632177faf3fdf253e17f07958aa
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2024-10-08 10:04:30 +0700

    update Navbar in Home

---
+++ pscp_project/oursite/booking/static/booking/Js/nav.js
@@ -0,0 +1,16 @@
+
+const menuIcon = document.getElementById('menu-icon');
+const menuImg = document.getElementById('menu-img');
+const navLinks = document.getElementById('nav-links');
+menuIcon.addEventListener('click', function() {
+    if (navLinks.classList.contains('show')) {
+        navLinks.classList.remove('show');
+        menuImg.src = "../../static/booking/icon/menu.png";
+        menuImg.classList.remove('rotate');
+    } else {
+        navLinks.classList.add('show');
+        menuImg.src = "../../static/booking/icon/close.png";
+        menuImg.classList.add('rotate');
+    }
+});
+
