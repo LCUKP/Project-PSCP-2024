88dc7073318d9a2200176cd838b700fcfbe8ed6e
LCUKP <puwanatjaijitjaam@gmail.com>
2024-10-09 00:42:43 +0700

    add page and routing

--- oursite/app_user/urls.py
+++ oursite/app_user/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from app_user import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('profile',views.profile),
+    path('booking',views.booking),
+    path('contact',views.contact),
+    path('report',views.report),
 ]
--- oursite/app_user/views.py
+++ oursite/app_user/views.py
@@ -7,3 +7,15 @@ def index(request) :
     all_user = user.objects.all()
     user2 = user.objects.filter(userid = 2)
     return render(request,"index.html",{"all_user":all_user,"user2":user2})
+
+def profile(request) :
+    return render(request,"profile.html")
+
+def booking(request) :
+    return render(request,"booking.html")
+
+def contact(request) :
+    return render(request,"contact.html")
+
+def report(request) :
+    return render(request,"report.html")
