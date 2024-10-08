2bade933cadc3eef0310e570a848ca8dddc417c4
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2024-10-08 22:19:56 +0700

    Update Navbar
    Add responsive and login form as popup.

--- oursite/app_user/static/Js/nav.js
+++ oursite/app_user/static/Js/nav.js
@@ -4,11 +4,11 @@ const navLinks = document.getElementById('nav-links');
 menuIcon.addEventListener('click', function() {
     if (navLinks.classList.contains('show')) {
         navLinks.classList.remove('show');
-        menuImg.src = "../../static/booking/icon/menu.png";
+        menuImg.src = "../static/icon/menu.png";
         menuImg.classList.remove('rotate');
     } else {
         navLinks.classList.add('show');
-        menuImg.src = "../../static/booking/icon/close.png";
+        menuImg.src = "../static/icon/close.png";
         menuImg.classList.add('rotate');
     }
 });
