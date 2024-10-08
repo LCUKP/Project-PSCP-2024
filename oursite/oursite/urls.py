17c1b3dcba0070901df109b5f35ef6d8b0b7f7d9
LCUKP <puwanatjaijitjaam@gmail.com>
2024-10-09 00:41:22 +0700

    add admin side

--- oursite/oursite/urls.py
+++ oursite/oursite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminis/', admin.site.urls),
     path('', include("app_user.urls")),
+    path('admin/', include("app_admin.urls")),
 ]
