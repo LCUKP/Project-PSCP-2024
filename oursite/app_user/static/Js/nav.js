46e74654181abeb48bdb86a0459f0d5d350ed1ab
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2024-10-09 15:42:36 +0700

    Update Navbar

--- oursite/app_user/static/Js/nav.js
+++ oursite/app_user/static/Js/nav.js
@@ -1,14 +1,34 @@
 const menuIcon = document.getElementById('menu-icon');
-const menuImg = document.getElementById('menu-img');
-const navLinks = document.getElementById('nav-links');
-menuIcon.addEventListener('click', function() {
-    if (navLinks.classList.contains('show')) {
-        navLinks.classList.remove('show');
-        menuImg.src = "../static/icon/menu.png";
-        menuImg.classList.remove('rotate');
-    } else {
-        navLinks.classList.add('show');
+const navLink = document.getElementById('nav-links');
+const menuImg = document.getElementById('menu-img')
+
+menuIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+    navLink.classList.toggle('show');
+
+    if (navLink.classList.contains('show')) {
         menuImg.src = "../static/icon/close.png";
         menuImg.classList.add('rotate');
+    } 
+    else {
+        menuImg.src = "../static/icon/menu.png";
+        menuImg.classList.remove('rotate');
+    }
+    event.stopPropagation();
+});
+
+document.addEventListener('click', function(event) {
+    if (!navLink.contains(event.target) && !menuIcon.contains(event.target)) {
+        navLink.classList.remove('show');
+        menuImg.src = "../static/icon/menu.png";
+        menuImg.classList.remove('rotate');
+    }
+});
+
+const overlay = document.getElementById('popup');
+
+overlay.addEventListener('click', function(event) {
+    if (event.target === overlay) {
+        window.location.hash = '';
     }
 });
