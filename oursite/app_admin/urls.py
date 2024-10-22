77de47d079beb235c177b5071a3e5ae64bf3c1fd
jjugumi <151422246+wednesdayvibe@users.noreply.github.com>
2024-10-22 15:26:21 +0700

    สร้าง form category

--- oursite/app_admin/urls.py
+++ oursite/app_admin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app_admin import views
 
 urlpatterns = [
-    path("",views.index)
+    path("",views.index),
+    path("add-category",views.form_cat)
 ]
--- oursite/app_admin/views.py
+++ oursite/app_admin/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) :
-    return render(request,"adminpage.html")
+    return render(request,"adminpage.html") 
+def form_cat(request) :
+    return render(request,"forms/category.html")
