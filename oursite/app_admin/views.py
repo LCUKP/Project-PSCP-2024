b15ec5ad873ee2e18283fb77ecd8dbc4ac36eb49
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2024-10-23 10:59:42 +0700

    Fix responsive margin bottom at contact.css

--- oursite/app_admin/views.py
+++ oursite/app_admin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) :
-    return render(request,"adminpage.html") 
+    return render(request,"adminpage.html")
 def form_cat(request) :
     return render(request,"forms/category.html")
 def form_room(request) :
