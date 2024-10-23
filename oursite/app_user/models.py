361c86710c8feeb9008fccd1f8511302255e3c5c
LCUKP <puwanatjaijitjaam@gmail.com>
2024-10-23 17:47:40 +0700

    remake models for user

--- oursite/app_user/models.py
+++ oursite/app_user/models.py
@@ -1,37 +1,40 @@
 from django.db import models
 from app_admin.models import room
 
-# Create your models here.
-class user(models.Model) :
-    userid = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    
-    def __str__(self) :
-        return  str(self.userid)+ " " + self.username + " " + self.lastname
-    
-class booking(models.Model) :
-    bookid = models.AutoField(primary_key=True)
-    datebook = models.DateField(null=False)
-    timebook = models.TimeField(null=False)
-    userid = models.ForeignKey(user,on_delete=models.CASCADE)
-    roomid = models.ForeignKey(room,on_delete=models.CASCADE)
-    
-    def __str__(self) :
-        return  str(self.bookid)+ " " + self.roomid + " " + self.datebook 
-
+# # Create your models here.
 class faculty(models.Model) :
     facid = models.AutoField(primary_key=True)
     facname = models.CharField(max_length=200)
-    
+
     def __str__(self) :
         return  str(self.facid)+ " " + self.facname
     
 class major(models.Model) :
-    majorid = models.AutoField(primary_key=True)
     majorname = models.CharField(max_length=200)
     facid = models.ForeignKey(faculty,on_delete=models.CASCADE)
     
     def __str__(self) :
-        return  str(self.majorid)+ " " + self.majorname
+        return  self.majorname
+
+class user(models.Model) :
+    code = models.CharField(max_length=13)
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    img = models.ImageField(upload_to ='app_user/static/img/')
+    faculty = models.ForeignKey(faculty,on_delete=models.CASCADE)
+    major = models.ForeignKey(major,on_delete=models.CASCADE)
+    
+    def __str__(self) :
+        return  self.code + " " + self.username + " " + self.lastname
+    
+class booking(models.Model) :
+    datebook = models.DateField(null=False)
+    timebook = models.TimeField(null=False)
+    reason = models.CharField(max_length=200,null=True)
+    user = models.ManyToManyField(user)
+    room = models.ManyToManyField(room)
+    
+    def __str__(self) :
+        return  str(self.bookid)+ " " + self.roomid + " " + self.datebook 
--- oursite/app_user/views.py
+++ oursite/app_user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app_user.models import user,faculty
-from app_admin.models import admin as assmin
+from app_admin.models import admin_acc
 
 # Create your views here.
 def index(request) :
@@ -16,7 +16,7 @@ def booking(request) :
     return render(request,"booking.html")
 
 def contact(request) :
-    admins = assmin.objects.all()
+    admins = admin_acc.objects.all()
     return render(request,"contact.html",{"admins":admins})
 
 def report(request) :
